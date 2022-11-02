a9af808dc6385b2df79aa57519a9b660a07bea19
ali <ali@celia.org>
2022-11-02 17:05:32 +0100

    config.php connection information

--- config/config.php
+++ config/config.php
@@ -1,3 +1,9 @@
 <?php
-    define('DBHOST','');
-    ?>
+    // Database configuration information
+    // This code just defines de constant DBHOST, DBUSER, DBPASS and DBNAME which are needed to connect to the database. 
+    // In this way when we need to change the connection parameters , it is enough to change this informations...
+    define('DBHOST','mardiadb');
+    define('DBUSER','root');
+    define('DBPASS','bitnami');
+    define('DBNAME','reservas');
+
