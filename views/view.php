0f682929f61c3b0c379234fb5f33b21597ee4107
ali <ali@celia.org>
2022-11-04 14:02:16 +0100

    view pattern

---
+++ views/view.php
@@ -0,0 +1,12 @@
+<?php
+
+// Template (Pattern) of views
+
+class View {
+    public static function show($viewName, $data = null) {
+        include("views/header.php");
+        include("views/nav.php");
+        include("views/$viewName.php");
+        include("view/footer.php");
+    }
+}
